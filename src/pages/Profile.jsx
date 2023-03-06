cde3332567afec6a72aa8e4c7eb45a516b72bccd
Jayro <jayroap27@gmail.com>
2023-03-06 18:08:48 -0300

    tela de perfil sem tests

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,8 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 
 export default function Profile() {
+  const [email, setEmail] = useState('');
+  const history = useHistory();
+
+  useEffect(() => {
+    if (localStorage.getItem('user')) {
+      const user = JSON.parse(localStorage.getItem('user'));
+      setEmail(user.email);
+    }
+  }, []);
+
+  const DoLogout = () => {
+    localStorage.clear();
+    history.push('/');
+  };
+
   return (
-    <Header title="Profile" />
+    <div>
+      <Header title="Profile" />
+      <p data-testid="profile-email">{email}</p>
+      <button
+        type="button"
+        data-testid="profile-done-btn"
+        onClick={ () => history.push('/done-recipes') }
+      >
+        Done Recipes
+      </button>
+      <button
+        type="button"
+        data-testid="profile-favorite-btn"
+        onClick={ () => history.push('/favorite-recipes') }
+      >
+        Favorite Recipes
+      </button>
+      <button
+        type="button"
+        data-testid="profile-logout-btn"
+        onClick={ DoLogout }
+      >
+        Logout
+      </button>
+    </div>
   );
 }
